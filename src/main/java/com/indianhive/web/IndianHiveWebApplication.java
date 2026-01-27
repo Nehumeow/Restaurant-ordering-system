f760b01ccc12770b25109e4865644701d57000ef
nc222jq <nc222jq@student.lnu.se>
2026-01-27 06:30:50 +0100

    Initial setup: Project skeleton and User entity

---
+++ src/main/java/com/indianhive/web/IndianHiveWebApplication.java
@@ -0,0 +1,13 @@
+package com.indianhive.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndianHiveWebApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IndianHiveWebApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/indianhive/web/IndianHiveWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indianhive.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndianHiveWebApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
